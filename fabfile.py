cfcd366a019a027f221ce45b2c2fbe9e2ec6304e
gamykla <admin@gamykla.com>
2016-09-05 15:18:07 -0400

    fix

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,11 @@ from fabric.api import task, run, local, settings, cd
 
 
 @task
-def pull_code():
+def sync_code():
     """
-    fab -u pi -i ~/keys/rpi/id_rsa pull_code
+    fab -u pi -i ~/keys/rpi/id_rsa sync_code
     """
+    local('git add .;git commit; git push;')
     with settings(host_string="raspberry.pi"):
         with cd('/home/pi/src/rpi'):
             run('git pull')
