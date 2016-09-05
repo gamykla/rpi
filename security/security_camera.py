63d5a0cbe4c179cfdc90adb1a5c74cd2bb5918b7
gamykla <admin@gamykla.com>
2016-09-05 15:12:26 -0400

    motion detection

--- security/security_camera.py
+++ security/security_camera.py
@@ -1,3 +1,4 @@
+import logging
 from io import BytesIO
 from time import sleep
 
@@ -6,22 +7,38 @@ from picamera import PiCamera
 
 import motion_detector
 
+logger = logging.getLogger(__name__)
 
-stream = BytesIO()
 
-
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-sleep(2)
-camera.capture(stream, format='jpeg')
-
-stream.seek(0)
-image = Image.open(stream)
-
+def _build_camera():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    return camera
 
 def main():
-    pass
+    camera = _build_camera()
+    camera.start_preview()
+    sleep(2)
+
+    last_image = None
+
+    while True:
+        sleep(1)
+        stream = BytesIO()
+        camera.capture(stream, format='jpeg')
+        stream.seek(0)
+
+        image = Image.open(stream)
+        if not last_image:
+            last_image = image
+
+        if motion_detector.is_motion_detected(image, last_image):
+            logger.debug("Motion is detected!")
+        else:
+            logger.debug("No motion.")
+
+        last_image = image
+
 
 if __name__ == "__main__":
     main()
