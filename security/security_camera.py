5444e1ab570fd93cc05c3af5e5f4d44ecc7525d6
gamykla <admin@gamykla.com>
2016-09-06 21:31:32 -0400

    clear resources

--- security/security_camera.py
+++ security/security_camera.py
@@ -36,27 +36,31 @@ class SecurityCamera():
         return self.motion_sensor.is_motion_detected(self.last_image_captured, new_image_captured)
 
     def capture_loop(self):
-        self.camera.start_preview()
-        sleep(2)
+        try:
+            self.camera.start_preview()
+            sleep(2)
 
-        while True:
-            sleep(0.5)
-            logger.debug("Capturing image.")
+            while True:
+                sleep(0.5)
+                logger.debug("Capturing image.")
 
-            stream = BytesIO()
-            self.camera.capture(stream, format='jpeg')
+                stream = BytesIO()
+                self.camera.capture(stream, format='jpeg')
 
-            stream.seek(0)
-            captured_image = Image.open(stream)
-            captured_image.load()
-            stream.close()
+                stream.seek(0)
+                captured_image = Image.open(stream)
+                captured_image.load()
+                stream.close()
 
-            if self._is_motion_detected(captured_image):
-                logger.debug("Motion detected.")
-            else:
-                logger.debug("No motion.")
+                if self._is_motion_detected(captured_image):
+                    logger.debug("Motion detected.")
+                else:
+                    logger.debug("No motion.")
 
-            self.last_image_captured = captured_image
+                self.last_image_captured = captured_image
+        finally:
+            logger.debug("Shutting down SecurityCamera.")
+            self.camera.close()
 
 
 def main():
