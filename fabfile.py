b618e6baa3202c8ac7b40b09bc1e715c18c1106e
gamykla <admin@gamykla.com>
2016-09-22 21:04:23 -0400

    add user

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ def push_to_pi():
     """
     test()
     local('git add .;git commit; git push;')
-    with settings(host_string="raspberry.pi"):
+    with settings(user="pi", host_string="raspberry.pi"):
         with cd('/home/pi/src/rpi'):
             run('git pull')
 
